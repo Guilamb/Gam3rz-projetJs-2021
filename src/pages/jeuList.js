cb5bf00db03a1a19cf624845d075840ce4bc3e02
Aubrian Duhayon <aubrian.duhayon.etu@univ-lille.fr>
2021-03-02 11:59:31 +0100

    update jeuList

--- src/pages/jeuList.js
+++ src/pages/jeuList.js
@@ -11,14 +11,17 @@ export default class jeuList extends Page {
 
 	set jeux(value) {
 		this.#jeux = value;
-		this.children = this.#jeux.map(jeu => new jeuThumbnail(jeu));
+		this.children = [this.#jeux].map(jeu => new jeuThumbnail(jeu));
 	}
 
 	mount(element) {
 		super.mount(element);
 		fetch('https://api.rawg.io/api/games')
 			.then(response => response.json())
-			.then(data => (this.jeux = data))
+			.then(data => {
+				this.jeux = data;
+				console.log(data);
+			})
 			.then(() => {
 				this.element.innerHTML = this.render();
 			});
