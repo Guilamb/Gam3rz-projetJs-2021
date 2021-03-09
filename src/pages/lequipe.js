86512dd644325d049e99d8aed642b6a1518a3481
Antoine Martinsse <antoine.martinsse.etu@univ-lille.fr>
2021-03-09 21:54:31 +0100

    page lequipe.fr finie

--- src/pages/lequipe.js
+++ src/pages/lequipe.js
@@ -1,4 +1,25 @@
 import Page from './Page';
 import Member from '../components/Member';
+import Component from '../components/Component';
 
-export default class Lequipe extends Page {}
+export default class Lequipe extends Page {
+	constructor() {
+		super([
+			new Component(
+				'h2',
+				{ name: 'class', value: 'title' },
+				"L'équipe de développement :"
+			),
+			new Member(
+				'Antoine',
+				'Martinsse',
+				'CorwynJ',
+				'Kirby Super Star Ultra',
+				'100%'
+			),
+			new Member('Aubrian', 'Duhayon', 'Skyeaaa', 'Fornite', '4000%'),
+			new Member('Guilhane', 'Bourgoin', 'Guilamb', '?', '100%'),
+			new Member('Thomas', 'Prunier', 'CosmicRadiocity', '?', '100%'),
+		]);
+	}
+}
