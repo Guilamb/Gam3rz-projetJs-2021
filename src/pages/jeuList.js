7a79d8390e32bdb63f5633224c4026407b6a99f4
Antoine MARTINSSE <martinsa@frene11.iut-infobio.priv.univ-lille1.fr>
2021-03-09 15:04:44 +0100

    Recherche

--- src/pages/jeuList.js
+++ src/pages/jeuList.js
@@ -1,5 +1,6 @@
 import Page from './Page';
 import jeuThumbnail from '../components/jeuThumbnail';
+import Router from '../Router';
 
 export default class jeuList extends Page {
 	#jeux;
@@ -14,47 +15,44 @@ export default class jeuList extends Page {
 		this.children = this.#jeux.map(jeu => new jeuThumbnail(jeu));
 	}
 
-	mount(element) {
-		super.mount(element);
-		for (let index = 1; index < 10; index++) {
-			fetch(`https://api.rawg.io/api/games?page=${index}`)
-				.then(response => response.json())
-				.then(data => {
-					this.jeux = data.results;
-				})
-				.then(() => {
-					this.element.innerHTML += this.render();
-				})
-				.then(() => {
-					document.querySelectorAll('.flip-card-inner').forEach(el => {
-						el.querySelector('.flip-card-back > button').addEventListener(
-							'click',
-							function (event) {
-								event.preventDefault();
-								const img = el.querySelector('.flip-card-back .fav');
+	show(url) {
+		fetch(url)
+			.then(response => response.json())
+			.then(data => {
+				this.jeux = data.results;
+			})
+			.then(() => {
+				this.element.innerHTML = this.render();
+			})
+			.then(() => {
+				document.querySelectorAll('.flip-card-inner').forEach(el => {
+					el.querySelector('.flip-card-back > button').addEventListener(
+						'click',
+						function (event) {
+							event.preventDefault();
+							const img = el.querySelector('.flip-card-back .fav');
 
-								if (!img.classList.contains('.click')) {
-									img.setAttribute('src', './images/star-fav-clicked.png');
-									img.classList.add('.click');
-									el.querySelector('.flip-card-front > .fav').setAttribute(
-										'src',
-										'./images/star-fav-clicked.png'
-									);
-									el.querySelector('.flip-card-front > .fav').removeAttribute(
-										'style'
-									);
-								} else {
-									img.setAttribute('src', './images/star-fav.png');
-									img.classList.remove('.click');
-									el.querySelector('.flip-card-front > .fav').setAttribute(
-										'style',
-										'visibility:hidden;'
-									);
-								}
+							if (!img.classList.contains('.click')) {
+								img.setAttribute('src', './images/star-fav-clicked.png');
+								img.classList.add('.click');
+								el.querySelector('.flip-card-front > .fav').setAttribute(
+									'src',
+									'./images/star-fav-clicked.png'
+								);
+								el.querySelector('.flip-card-front > .fav').removeAttribute(
+									'style'
+								);
+							} else {
+								img.setAttribute('src', './images/star-fav.png');
+								img.classList.remove('.click');
+								el.querySelector('.flip-card-front > .fav').setAttribute(
+									'style',
+									'visibility:hidden;'
+								);
 							}
-						);
-					});
+						}
+					);
 				});
-		}
+			});
 	}
 }
