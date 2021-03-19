44b820d201e23b660ca14399f059a912f0b8b9a9
Aubrian Duhayon <aubrian.duhayon.etu@univ-lille.fr>
2021-03-19 16:06:31 +0100

    Fin design page Détails

--- src/pages/details.js
+++ src/pages/details.js
@@ -33,7 +33,7 @@ export default class Details extends Page {
 		if (!data && !gameImages) return 'Chargement en cours';
 
 		const video = data.clip
-			? `<video controls="controls"> <source src="${data.clip.clip}"/> </video>`
+			? `<video controls="controls" onloadstart="this.volume=0"> <source src="${data.clip.clip}"/> </video>`
 			: `<img src='https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif'>`;
 
 		let images = '';
@@ -56,36 +56,45 @@ export default class Details extends Page {
 		genres += '</ul>';
 
 		return `
+        <link rel="stylesheet" type="text/css" href="css/pageDetails.css" />
 		<div>
 			<br>
 			<div id="title">
-				<h1> ${data.name} NOTE : ${data.metacritic}</h1>
+				<h1 class="name"> ${data.name} </h1>
+				<h1 class="note"> ${data.metacritic} </h1>
 			</div>
 			<br>
-			<div id="video">
-				Vidéo de présentation
-				${video}
+			<div id="presentation">
+				<h1> <strong> Vidéo de présentation </strong> </h1>
+				<br>
+				<div id="video">
+					${video}
+				</div>
+			</div>
+			<br>
+			<div id="description">
+				<h1> <strong> Description du jeu </strong> </h1>
+				<br>
+				${data.description}
 			</div>
 			<br>
 			<div id="screenshots">
-				Liste de screenshots
+				<h1> <strong> Screenshots </strong> </h1>
+				<br>
 				<figure>
 					${images}
 				</figure>
 			</div>
 			<br>
-			<div id="description">
-				Description
-				${data.description}
-			</div>
-			<br>
 			<div id="plateformes">
-				Plateformes
+				<h1> <strong> Plateformes </strong> </h1>
+				<br>
 				${plateformes}
 			</div>
 			<br>
 			<div id="genres">
-				Genres
+				<h1> <strong> Genres </strong> </h1>
+				<br>
 				${genres}
 			</div>
 			<br>
