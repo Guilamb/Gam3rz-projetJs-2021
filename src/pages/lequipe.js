beb1c50db815cd4885030676d7d7b6d93a47c52e
Antoine Martinsse <antoine.martinsse.etu@univ-lille.fr>
2021-03-21 15:04:45 +0100

    modification lequipe

--- src/pages/lequipe.js
+++ src/pages/lequipe.js
@@ -4,28 +4,38 @@ import Component from '../components/Component';
 
 export default class Lequipe extends Page {
 	constructor() {
-		super([
-			new Component(
-				'h2',
-				{ name: 'class', value: 'title' },
-				"L'équipe de développement :"
-			),
-			new Member(
-				'Antoine',
-				'Martinsse',
-				'CorwynJ',
-				'Kirby-Super-Star-Ultra',
-				'100%'
-			),
-			new Member('Aubrian', 'Duhayon', 'Skyeaaa', 'Dying-Light', '4000%'),
-			new Member('Guilhane', 'Bourgoin', 'Guilamb', 'hollow-knight', '100%'),
-			new Member(
-				'Thomas',
-				'Prunier',
-				'CosmicRadiocity',
-				'the-legend-of-zelda-the-wind-waker-hd',
-				'100%'
-			),
-		]);
+		super();
+	}
+
+	render() {
+		return `
+		<link rel="stylesheet" type="text/css" href="css/lequipe.css" />
+		<h2>L'équipe de développement :</h2>
+		
+		<div class="row align-items-start">
+		<div class="member">
+		<h6 class="nom">Antoine Martinsse CorwynJ</h6>
+		<a href="/details-Kirby-Super-Star-Ultra">Son jeu préféré est Kirby-Super-Star-Ultra</a>
+		<h6 class="note">Il mérite un bon 100% de la note</h6>
+		</div>
+		
+		<div class="member">
+		<h6 class="nom">Aubrian Duhayon Skyeaaa</h6>
+		<a href="/details-Dying-Light">Son jeu préféré est Dying-Light</a>
+		<h6 class="note">Il mérite un bon 4000% de la note</h6>
+		</div>
+
+		<div class="member">
+		<h6 class="nom">Guilhane Bourgoin Guilamb</h6>
+		<a href="/details-hollow-knight">Son jeu préféré est hollow-knight</a>
+		<h6 class="note">Il mérite un bon 100% de la note</h6>
+		</div>
+
+		<div class="member">
+		<h6 class="nom">Thomas Prunier CosmicRadiocity</h6>
+		<a href="/details-the-legend-of-zelda-the-wind-waker-hd">Son jeu préféré est the-legend-of-zelda-the-wind-waker-hd</a>
+		<h6 class="note">Il mérite un bon 100% de la note</h6>
+		</div>
+		</div>`;
 	}
 }
