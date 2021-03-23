85183a0fcb9b0990490cbab18ee3b368a6f0b52a
Aubrian Duhayon <aubrian.duhayon.etu@univ-lille.fr>
2021-03-23 16:55:39 +0100

    Résolution bug liste favo null

--- src/components/request.js
+++ src/components/request.js
@@ -24,10 +24,10 @@ export default class Requete {
 				jeux = data.results;
 			})
 			.then(() => {
-				if (!add && page.element) {
+				if (!add) {
 					page.element.innerHTML = page.render();
 				} else {
-					if (jeux && page.element) page.element.innerHTML += page.render();
+					page.element.innerHTML += page.render();
 				}
 			})
 			.then(() => {
@@ -37,7 +37,7 @@ export default class Requete {
 						const button_favorite = el.querySelector(
 							'#gameCard-button-favorite'
 						);
-						let cliqued;
+						let cliqued = false;
 						button_favorite.addEventListener('click', function (event) {
 							event.preventDefault();
 							if (
@@ -51,7 +51,6 @@ export default class Requete {
 							const whatIsInLocalStorage = localStorage.getItem('favoris');
 
 							if (cliqued) {
-								console.log('ajout');
 								button_favorite
 									.querySelector('img')
 									.setAttribute('src', 'images/fav-clicked.png');
@@ -64,23 +63,10 @@ export default class Requete {
 
 								localStorage.setItem('favoris', JSON.stringify(listeFavoris));
 							} else {
-								console.log('retrait');
 								button_favorite
 									.querySelector('img')
 									.setAttribute('src', 'images/fav.png');
-
-								if (whatIsInLocalStorage != null) {
-									listeFavoris = JSON.parse(whatIsInLocalStorage);
-								}
-								let myIndex = 0;
-								let i = 0;
-								listeFavoris.forEach(game => {
-									if (game.name == jeux[index].name) {
-										myIndex = i;
-									}
-									i++;
-								});
-								listeFavoris.splice(myIndex, 1);
+								listeFavoris.splice(listeFavoris.indexOf(jeux[index]), 1);
 								localStorage.removeItem('favoris');
 								localStorage.setItem('favoris', JSON.stringify(listeFavoris));
 							}
